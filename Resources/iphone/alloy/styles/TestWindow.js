e617f1cedbed9843645afad51df22176bdd3affc
Jesse.Newcomer <jesse.newcomer@gmail.com>
2014-01-10 10:57:12 -0500

    removed Resources

--- Resources/iphone/alloy/styles/TestWindow.js
+++
@@ -1,1 +0,0 @@
-module.exports = [{"isApi":true,"priority":1000.0001,"key":"Window","style":{backgroundColor:"white",}},{"isApi":true,"priority":1000.0003,"key":"Button","style":{top:20,width:200,height:50,}}];
